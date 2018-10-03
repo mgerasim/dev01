ee6bf0958ae4a25d48e01fc6bc4698d7a6c21ff4
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-10-03 05:02:43 +0300

    Create migrate sql by PgMigrator

--- migrate.sh
+++ migrate.sh
@@ -2,13 +2,13 @@
 M2PG_CONF=MsSqlAWT2Postgres.conf
 
 M2PG_PROP=
-M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.user=readonly
-M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.password=secret
+M2PG_PROP=$M2PG_PROP -Dconnections.mssql.user=$SQLSERVER_USERNAME
+M2PG_PROP=$M2PG_PROP -Dconnections.mssql.password=$SQLSERVER_PASSWORD
 
-M2PG_CLASSPATH="lib/*"
+M2PG_CLASSPATH="/usr/src/sqljdbc_2.0/enu/*:/home/dev/projects/Migrate2Postgres/target/lib/*:/home/dev/projects/Migrate2Postgres/target/Migrate2Postgres-1.0.7.jar"
 
 ## get the command from the first arg passed, must be either DDL or DML
 M2PG_COMMAND=$1
 
 
-java $M2PG_PROP -cp $M2PG_CLASSPATH net.twentyonesolutions.m2pg.PgMigrator $M2PG_COMMAND $M2PG_CONF
+java -Dconnections.mssql.user=sa -cp $M2PG_CLASSPATH net.twentyonesolutions.m2pg.PgMigrator $M2PG_COMMAND $M2PG_CONF
