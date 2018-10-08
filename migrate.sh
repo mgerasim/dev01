aefe8079a1a7886ef6dfe09eb1cbb0b7147c1c30
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-10-09 01:44:10 +0300

    Миграция данных

--- migrate.sh
+++ migrate.sh
@@ -5,7 +5,7 @@ M2PG_PROP=
 M2PG_PROP=$M2PG_PROP -Dconnections.mssql.user=$SQLSERVER_USERNAME
 M2PG_PROP=$M2PG_PROP -Dconnections.mssql.password=$SQLSERVER_PASSWORD
 
-M2PG_CLASSPATH="/usr/src/sqljdbc_2.0/enu/*:/home/dev/projects/Migrate2Postgres/target/lib/*:/home/dev/projects/Migrate2Postgres/target/Migrate2Postgres-1.0.7.jar"
+M2PG_CLASSPATH="/usr/src/sqljdbc_2.0/enu/*:/home/dev/projects/Migrate2Postgres/target/lib/*:/home/dev/projects/Migrate2Postgres/target/Migrate2Postgres-1.0.7.jar:/usr/share/java/postgresql.jar"
 
 ## get the command from the first arg passed, must be either DDL or DML
 M2PG_COMMAND=$1
--- sqlserver2pgsql.sh
+++ sqlserver2pgsql.sh
@@ -2,4 +2,15 @@ perl ../sqlserver2pgsql/sqlserver2pgsql.pl 	-f SQLSERVER.dump \
 						-b pgsql_out_sqlserver2pgsql_01.sql \
 						-a pgsql_out_sqlserver2pgsql_02.sql \
 						-u pgsql_out_sqlserver2pgsql_03.sql \
+                                                -k out \
+                                                -sd localhost \
+                                                -sh localhost \
+                                                -sp 1433 \
+                                                -su su \
+                                                -sw ex6ectDFMJLOwbEnAIR0 \
+                                                -pd lers \
+                                                -ph localhost \
+                                                -pp 5432 \
+                                                -pu lers \
+                                                -pw ILyS3L5oEfOUubJX0a2k \
                                                 -keep_identifier_case > pgsql_out_sqlserver2pgsql_04.log 2>&1
